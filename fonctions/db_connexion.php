3f50f5414daea0b6a0c94f7973d774be3d75065f
Leclerc <killonea@hotmail.fr>
2018-05-19 20:41:41 +0200

    correction du nom de la bdd

--- fonctions/db_connexion.php
+++ fonctions/db_connexion.php
@@ -2,7 +2,7 @@
 	// Créations des variables contenant toutes les informations de connexion .
 	$host = "localhost";
 	$user = "root";
-	$bdd	= "terrabay";
+	$bdd	= "bdd_terrabay";
 	$password = "";
 	// Tentative d'acces a la base de donnée .
 	$db_connexion = mysqli_connect($host , $user , $password , $bdd) or die ("Erreur de connexion a la bade de donnée ! ");
