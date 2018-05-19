2b7955ac2e11df3a6c6a0153f21e7cf38bc73b95
Leclerc <killonea@hotmail.fr>
2018-05-19 14:03:02 +0200

    code du home réarengé

--- home.php
+++ home.php
@@ -3,10 +3,31 @@
 	if (empty($_SESSION["pseudo"])){
 		header("location:index.php");
 	}
-	echo "<form action='fonctions/disconnect.php' method='GET'>
-					<button>disconnect</button></form>";
 
+
+
+// ne pas mettre des trucs incomplets pls si vous faites une fonctionnalité
+// assurez vous qu'elle marche un minimum
+/*
 	if ( $_GET["action"] === "disconnect"){
 		echo "banana";
 	}
+*/
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+	<?php
+		echo "<form action='fonctions/disconnect.php' method='GET'> 
+		<button>disconnect</button></form>";
+	?>
+</body>
+</html>
+	
+
