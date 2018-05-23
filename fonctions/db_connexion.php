8d27ef27553327c1095326a7799920077f551d34
Guillaume <Guillaume.cyril.zisa@gmail.com>
2018-05-23 10:08:58 +0200

    short_update

--- fonctions/db_connexion.php
+++ fonctions/db_connexion.php
@@ -1,9 +1,9 @@
 <?php
-	// Créations des variables contenant toutes les informations de connexion .
+	// CRÉATION DES VARIABLES DE CONNEXION A LA BASE DE DONNÉE
 	$host = "localhost";
 	$user = "root";
 	$bdd	= "bdd_terrabay";
 	$password = "";
-	// Tentative d'acces a la base de donnée .
+	// TENTATIVE ACCES A LA BASE DE DONNÉE
 	$db_connexion = mysqli_connect($host , $user , $password , $bdd) or die ("Erreur de connexion a la bade de donnée ! ");
 ?>
