6654f5e3cca5604b176f7080c3840febd3e40b6e
Guillaume <Guillaume.cyril.zisa@gmail.com>
2018-05-23 10:05:09 +0200

    Update_account_with_commentary

--- account.php
+++ account.php
@@ -3,7 +3,6 @@
 	if (empty($_SESSION["pseudo"])){
 		header("location:index.php");
 	}
-
 ?>
 
 <!DOCTYPE html>
@@ -17,20 +16,34 @@
 		</form>
 
 		<?php
-
-		require_once("fonctions/db_connexion.php");
-		$req = mysqli_query($db_connexion, "SELECT * FROM users WHERE pseudo = '".$_SESSION["pseudo"]."'");
-		$data = mysqli_fetch_array($req);
-
-		echo "Pseudo :".$data["pseudo"]."<br>
-					Firstname".$data["firstname"]."<br>
-					Name :".$data["name"]."<br>
-					Note :".$data["note"]."<br>";
-
+			// AFFICHAGE DES INFORMATIONS ACTUELLES DE L'UTILISATEUR .
+			require_once("fonctions/db_connexion.php");
+			$req = mysqli_query($db_connexion, "SELECT * FROM users WHERE pseudo = '".$_SESSION["pseudo"]."'");
+			$data = mysqli_fetch_array($req);
+			echo "Pseudo :".$data["pseudo"]."<br>
+						Firstname :".$data["firstname"]."<br>
+						Name :".$data["name"]."<br>
+						Note :".$data["note"]."<br>";
 		?>
 
 		<form action='account_settings.php' method='GET'>
 			<button>Settings</button>
 		</form>
+
+		<?php
+			// COMMENTAIRE DE MODIFICATION
+			if (isset ( $_GET["done"])){
+				if ( $_GET["done"] == "new_pseudo"){
+					echo"<h6>Your pseudo has been changed .</h6>";
+				} elseif ( $_GET["done"] == "new_firstname"){
+					echo"<h6>Your firstname has been changed .</h6>";
+				} elseif ( $_GET["done"] == "new_name"){
+					echo"<h6>Your name has been changed .</h6>";
+				} elseif ( $_GET["done"] == "new_password"){
+					echo"<h6>Your password has been changed .</h6>";
+				}
+			}
+		?>
+
 	</body>
 </html>
--- fonctions/account_settings.php
+++ fonctions/account_settings.php
@@ -3,15 +3,16 @@
 	if (empty($_SESSION["pseudo"])){
 		header("location:index.php");
 	}
+	// LOCALISATION DU PSEUDO DANS LA BASE DE DONNÉE ET STOCKAGE DE SA LIGNE DANS $DATA
 	require_once("db_connexion.php");
-
-
 	$req = mysqli_query($db_connexion, "SELECT * FROM users WHERE pseudo = '".$_SESSION["pseudo"]."'");
 	$data = mysqli_fetch_array($req);
 
+	// IDENTIFICATION BDD PDO POUR MODIFICATIOIN
 	$mysqli = new mysqli("localhost", "root", "", "bdd_terrabay");
 
-  if (isset($_POST["pseudo"])){
+	// MODIFICATION DE DONNÉES
+	if (isset($_POST["pseudo"])){
 		$pseudo = htmlspecialchars($_POST["pseudo"]);
 		$req1 = mysqli_query($db_connexion, "SELECT * FROM users WHERE pseudo = '".$pseudo."'");
 		if ( mysqli_num_rows($req1) == 1 ){
@@ -21,20 +22,20 @@
 			$stmt = $mysqli->prepare("UPDATE users SET pseudo = ? WHERE id_user = ?");
 			$stmt->bind_param('sd', $pseudo, $data["id_user"]);
 			$stmt->execute();
-			header("location:../account.php");
+			header("location:../account.php?done=new_pseudo");
 		}
   } elseif(isset($_POST["firstname"])){
 		$firstname = htmlspecialchars($_POST["firstname"]);
 		$stmt = $mysqli->prepare("UPDATE users SET firstname = ? WHERE id_user = ?");
 		$stmt->bind_param('sd', $firstname, $data["id_user"]);
 		$stmt->execute();
-		header("location:../account.php");
+		header("location:../account.php?done=new_firstname");
   } elseif(isset ($_POST["name"])){
 		$name = htmlspecialchars($_POST["name"]);
 		$stmt = $mysqli->prepare("UPDATE users SET name = ? WHERE id_user = ?");
 		$stmt->bind_param('sd', $name, $data["id_user"]);
 		$stmt->execute();
-		header("location:../account.php");
+		header("location:../account.php?done=new_name");
   } elseif(isset ($_POST["password"]) || isset($_POST["password1"]) || isset($_POST["password2"])){
 		if(!isset ($_POST["password"]) || !isset($_POST["password1"]) || !isset($_POST["password2"])){
 			header("location:../account_settings.php?error=password_not_set");
@@ -47,7 +48,7 @@
 					$stmt = $mysqli->prepare("UPDATE users SET password = ? WHERE id_user = ?");
 					$stmt->bind_param('sd', $password2, $data["id_user"]);
 					$stmt->execute();
-					header("location:../account.php");
+					header("location:../account.php?done=new_password");
 				} else {
 					header("location:../account_settings.php?error=bad_password");
 				}
