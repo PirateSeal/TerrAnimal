93c711995d39e17cb36a2a8420f93040adc056b6
Leclerc <killonea@hotmail.fr>
2018-05-27 11:05:54 +0200

    Correction
    Correction au niveau de l'affichage des articles où un produit mis en vente par un utilisateur lui serai proposé à l'achat

--- model/home_model.php
+++ model/home_model.php
@@ -1,8 +1,13 @@
 <?php
 
 	$pdo = new PDO('mysql:host=localhost;dbname=bdd_terrabay','root','');
+	$sql ='select id_user from users where pseudo = "'.$_SESSION['pseudo'].'"';
+	$req = $pdo->query($sql);
+	$id = $req->fetch();
 
-	$sql = 'select id_article, unit_price, name from articles inner join species on articles.id_specie = species.id_specie where id_article;';
+
+
+	$sql = 'select id_article, unit_price, species.name from users inner join articles on users.id_user = articles.id_user inner join species on articles.id_specie = species.id_specie where users.id_user != "'.$id['id_user'].'"';
 	$req = $pdo->query($sql);
 
 	$i=0;
