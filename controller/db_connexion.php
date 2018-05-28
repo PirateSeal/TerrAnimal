5211b777eeddd37e79bd2999af611ae6371cd67e
root <guillaume.cyril.zisa@gmail.com>
2018-05-28 22:47:56 +0200

    again

--- controller/db_connexion.php
+++ controller/db_connexion.php
@@ -1,7 +1,7 @@
 <?php
 
   try{
-    $db_connexion=new PDO("mysql:host=localhost;dbname=bdd_terrabay","root","");
+    $db_connexion=new PDO("mysql:host=localhost;dbname=db_terrabay","root","");
   } catch(PDOException $e){
     echo "Failed to connect to the database .";
   }
