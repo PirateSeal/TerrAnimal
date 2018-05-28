6e697778470e57d0cbb3420c8b26543a7bf2e732
root <guillaume.cyril.zisa@gmail.com>
2018-05-28 23:27:32 +0200

    Home_controller_update

--- model/home_model.php
+++ model/home_model.php
@@ -1,11 +1,11 @@
 <?php
 
-	$pdo = new PDO('mysql:host=localhost;dbname=bdd_terrabay','root','');
+	require_once("../controller/db_connexion.php");
 	$sql ='select id_user,balance from users where pseudo = "'.$_SESSION['pseudo'].'"';
-	$req = $pdo->query($sql);
+	$req = $db_connexion->query($sql);
 	$user_data = $req->fetch();
 	$sql = 'select id_article, unit_price, species.name from users inner join articles on users.id_user = articles.id_user inner join species on articles.id_specie = species.id_specie where users.id_user != "'.$user_data['id_user'].'"';
-	$req = $pdo->query($sql);
+	$req = $db_connexion->query($sql);
 
 
 	$i=0;
