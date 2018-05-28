ccfbc1f1b115747713ad98383a159cfde915a211
root <guillaume.cyril.zisa@gmail.com>
2018-05-29 00:56:27 +0200

    BETTER_XOR

--- controller/xor.php
+++ controller/xor.php
@@ -1,27 +1,22 @@
 <?php
-$xor_key = 'ByTheWay66';
-function xorIt($string, $key, $type = 0)
-{
-        $sLength = strlen($string);
-        $xLength = strlen($key);
-        for($i = 0; $i < $sLength; $i++) {
-                for($j = 0; $j < $xLength; $j++) {
-                        if ($type == 1) {
-                                //decrypt
-                                $string[$i] = $key[$j]^$string[$i];
-
-                        } else {
-                                //crypt
-                                $string[$i] = $string[$i]^$key[$j];
-                        }
-                }
+  $xor_key = 'ByTheWay66';
+  function xorIt($string, $key, $type = 0){
+    $sLength = strlen($string);
+    $xLength = strlen($key);
+    for($i = 0; $i < $sLength; $i++) {
+      for($j = 0; $j < $xLength; $j++) {
+        if ($type == 1) {
+          //decrypt
+          $string[$i] = $key[$j]^$string[$i];
+        } else {
+          //crypt
+          $string[$i] = $string[$i]^$key[$j];
         }
-        return $string;
-}
-
+      }
+    }
+    return $string;
+  }
 $signal = base64_encode(xorIt($string, $xor_key));
-
-
 $string = xorIt(base64_decode($signal), $xor_key, 1);
 
 ?>
