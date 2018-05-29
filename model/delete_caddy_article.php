438df23b46acbf82901ad76b491d264946fe22f9
Leclerc <killonea@hotmail.fr>
2018-05-29 16:58:29 +0200

    Correction
    Correction d'une erreur ou l'article du panier à suprimer n'était pas celui choisie

--- model/delete_caddy_article.php
+++ model/delete_caddy_article.php
@@ -4,13 +4,18 @@
 		header("location:../index.php");
 	}else{
 		$data = $_COOKIE['toto'];
+		$data = rtrim($data, ";");
 		$data = explode(";", $data);
 		if (count($data)<2) {
 			setcookie("toto", $_GET["id"], time()-1,"/");
 			header("location:../controller/caddy_controller.php");
 		}else{
+			print_r($data);
+			print_r($_GET);
 			$id=$_GET["id"];
 			array_splice($data,$id,1);
+
+
 			$data = array_values($data);
 			$string ="";
 			for ($i=0; $i <count($data) ; $i++) {
