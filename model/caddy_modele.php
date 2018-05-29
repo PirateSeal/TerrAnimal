518ffcabc8eae6f97a1cbb27ad58bacc6be2b99c
Leclerc <killonea@hotmail.fr>
2018-05-29 09:40:27 +0200

    correction3

--- model/caddy_modele.php
+++ model/caddy_modele.php
@@ -1,17 +1,16 @@
 <?php
-
-	$pdo = new PDO('mysql:host=localhost;dbname=bdd_terrabay','root','');
+	require_once("../controller/db_connexion.php");
 
 	$data = $_COOKIE['toto'];
 	$data = explode(";", $data);
 	
 	$sql ='select id_user,balance from users where pseudo = "'.$_SESSION['pseudo'].'"';
-	$req = $pdo->query($sql);
+	$req = $db_connexion->query($sql);
 	$user_data = $req->fetch();
 
 	for ($i=0; $i < count($data); $i++) { 
 		$sql = 'select name, unit_price, id_article from articles inner join species on species.id_specie = articles.id_specie where id_article ='.$data[$i];
-		$req = $pdo->query($sql);
+		$req = $db_connexion->query($sql);
 		while($row = $req->fetch(PDO::FETCH_ASSOC)){
 			$recup[$i] = $row;
 		}
