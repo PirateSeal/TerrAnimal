03c639f65e90b99ed5c0bc1f3989b62c83e9755c
SibreuR <erwann1998@gmail.com>
2018-05-29 10:10:54 +0200

    correctino

--- model/add_article.php
+++ model/add_article.php
@@ -13,22 +13,24 @@
 
 	//RECUPERATION DE ID_USER GRACE AU PSEUDO
 
-	$req = mysqli_query($db_connexion, "SELECT id_user FROM users WHERE pseudo = '".$_SESSION["pseudo"]."'");
-	$data = mysqli_fetch_array($req, MYSQLI_NUM);
+	$sql = "SELECT id_user FROM users WHERE pseudo = '".$_SESSION["pseudo"]."'";
+	$req = $db_connexion->query($sql);
+	$data = $req -> fetch();
 	$id= $data[0];
-	mysqli_free_result($req);
 
 	//RECUPERATION DE ID_SPECIE GRACE AU NAME
 
-	$req2 = mysqli_query($db_connexion, "SELECT id_specie FROM species WHERE name = '".$_GET["name"]."'");
-	$data2 = mysqli_fetch_array($req2, MYSQLI_NUM);
+	$sql2 = "SELECT id_specie FROM species WHERE name = '".$_GET["name"]."'";
+	$req2 = $db_connexion->query($sql);
+	$data2 = $req2 -> fetch();
 	$specie = $data2[0];
 
 
 	if ( empty ( $descri ) || empty ( $price ) || $stock == '0' || empty ($size) || empty($color) ){
 		header("location:../view/add_article.php?error=incomplete");
 	} else {
-		mysqli_query($db_connexion, "INSERT INTO articles (id_article, id_specie, id_user, description, unit_price, stock, gender, diet, weight, size, color, age) VALUES (NULL, '".$specie."', '".$id."', '".$descri."', '".$price."', '".$stock."', '".$gender."', '".$diet."', '".$weight."', '".$size."', '".$color."', '".$age."')");
+		$sql= "INSERT INTO articles (id_article, id_specie, id_user, description, unit_price, stock, gender, diet, weight, size, color, age) VALUES (NULL, '".$specie."', '".$id."', '".$descri."', '".$price."', '".$stock."', '".$gender."', '".$diet."', '".$weight."', '".$size."', '".$color."', '".$age."')";
+		$sql->execute();
 		header("location:../view/add_article.php?validation=confirmed");
 	}
 ?>
