ae84c28beb9e28d7b813660e6e516d5ab48a607c
root <guillaume.cyril.zisa@gmail.com>
2018-05-30 14:28:13 +0200

    ACCOUNT_MODEL

--- controller/account_settings.php
+++ controller/account_settings.php
@@ -2,33 +2,38 @@
 	require_once("loged_or_not.php");
 	require_once("db_connexion.php");
 	require_once("xor.php");
-	$data = $db_connexion->query("SELECT * FROM users WHERE pseudo='".$_SESSION["pseudo"]."'")->fetch();
+	include("../model/account_model.php");
+	$data = $db_connexion->query($req_data)->fetch();
 
 	// MODIFICATION DE DONNÉES
 if (isset($_POST["pseudo"])){
 	$pseudo = htmlspecialchars($_POST["pseudo"]);
-	$how_much = $db_connexion->query("SELECT COUNT(*) FROM users WHERE pseudo='".$pseudo."' ")->fetch();
+	include("../model/account_model.php");
+	$how_much = $db_connexion->query($req_how_much)->fetch();
 	if( $how_much['COUNT(*)'] == 1 ){
 		header("location:../view/account_settings.php?error=pseudo_exist");
 	} else {
-		$req = $db_connexion->prepare("UPDATE users SET pseudo = '".$pseudo."' WHERE id_user = '".$data['id_user']."'");
+		$req = $db_connexion->prepare($req_pseudo);
 		$req->execute();
 		$_SESSION["pseudo"] = $pseudo ;
 		header("location:../view/account.php?done=new_pseudo");
 	}
 } elseif(isset($_POST["firstname"])){
 	$firstname = htmlspecialchars($_POST["firstname"]);
-	$req = $db_connexion->prepare("UPDATE users SET firstname = '".$firstname."' WHERE id_user = '".$data['id_user']."'");
+	include("../model/account_model.php");
+	$req = $db_connexion->prepare($req_firstname);
 	$req->execute();
 	header("location:../view/account.php?done=new_firstname");
 }elseif(isset($_POST["email"])){
 	$email = htmlspecialchars($_POST["email"]);
-	$req = $db_connexion->prepare("UPDATE users SET email = '".$email."' WHERE id_user = '".$data['id_user']."'");
+	include("../model/account_model.php");
+	$req = $db_connexion->prepare($req_name);
 	$req->execute();
 	header("location:../view/account.php?done=new_email");
 } elseif(isset($_POST["name"])){
 	$name = htmlspecialchars($_POST["name"]);
-	$req = $db_connexion->prepare("UPDATE users SET name = '".$name."' WHERE id_user = '".$data['id_user']."'");
+	include("../model/account_model.php");
+	$req = $db_connexion->prepare($req_email);
 	$req->execute();
 	header("location:../view/account.php?done=new_name");
 } elseif(isset ($_POST["password"]) || isset($_POST["password1"]) || isset($_POST["password2"])){
@@ -47,7 +52,8 @@ if (isset($_POST["pseudo"])){
 				$xor_key = 'ByTheWay66';
 				$signal = base64_encode(xorIt($password1, $xor_key));
 				$password = $signal;
-				$req = $db_connexion->prepare("UPDATE users SET password = '".$password."' WHERE id_user = '".$data['id_user']."'");
+				include("../model/account_model.php");
+				$req = $db_connexion->prepare();
 				$req->execute();
 				header("location:../view/account.php?done=new_password");
 			} else {
@@ -59,7 +65,8 @@ if (isset($_POST["pseudo"])){
 	}
 }
 if ( isset($_POST["account"]) && $_POST["account"] == "delete"){
-	$req = $db_connexion->prepare("DELETE FROM users WHERE id_user = '".$data['id_user']."'");
+	include("../model/account_model.php");
+	$req = $db_connexion->prepare($req_delete_account);
 	$req->execute();
 	header("location:disconnect.php");
 }
---
+++ model/account_model.php
@@ -0,0 +1,10 @@
+<?php
+  $req_data = "SELECT * FROM users WHERE pseudo='".$_SESSION["pseudo"]."'";
+  $req_how_much = "SELECT COUNT(*) FROM users WHERE pseudo='".$pseudo."' ";
+  $req_pseudo = "UPDATE users SET pseudo = '".$pseudo."' WHERE id_user = '".$data['id_user']."'" ;
+  $req_firstname = "UPDATE users SET firstname = '".$firstname."' WHERE id_user = '".$data['id_user']."'";
+  $req_name = "UPDATE users SET email = '".$email."' WHERE id_user = '".$data['id_user']."'";
+  $req_email = "UPDATE users SET name = '".$name."' WHERE id_user = '".$data['id_user']."'";
+  $req_passowrd = "UPDATE users SET password = '".$password."' WHERE id_user = '".$data['id_user']."'";
+  $req_delete_account = "DELETE FROM users WHERE id_user = '".$data['id_user']."'";
+?>
