c26583268f7d5a3186ead57f57ed957cc022c24b
root <guillaume.cyril.zisa@gmail.com>
2018-05-30 14:20:21 +0200

    MODELS

--- controller/subscribe.php
+++ controller/subscribe.php
@@ -1,12 +1,14 @@
 <?php
 	require_once("db_connexion.php");
 	require_once("xor.php");
+
 	$pseudo= htmlspecialchars($_GET["pseudo"]);
 	$firstname= htmlspecialchars($_GET["firstname"]);
 	$email = htmlspecialchars($_GET["email"]);
 	$name= htmlspecialchars($_GET["name"]);
 	$password1= htmlspecialchars($_GET["password1"]);
 	$password2= htmlspecialchars($_GET["password2"]);
+		require("../model/subscribe_model.php");
 
 		/*$user = [$pseudo,$firstname,$name,$email,$password1,$password2];
 		$interdit = ['"',"&","'","{","(","[","-","|","`","_","'\'"];
@@ -23,14 +25,13 @@
 		 if ( $flag === 1 ){
 			 header("location:../index.php?go=subscribe");
 		 }*/
-		 
+	$how_much = $db_connexion->query($req_subscribe)->fetch();
 	if( $how_much['COUNT(*)'] == 1 ){
 		header("location:../index.php?go=subscribe");
 	} else {
 		$xor_key = 'ByTheWay66';
 		$signal = base64_encode(xorIt($password1, $xor_key));
 		$password = $signal;
-
 		$db_connexion->exec($sql);
 		header("location:../index.php?subscribe=confirmed");
 	}
--- model/subscribe_model.php
+++ model/subscribe_model.php
@@ -1,5 +1,6 @@
 <?php
-$how_much = $db_connexion->query("SELECT COUNT(*) FROM users WHERE pseudo='".$pseudo."' ")->fetch();
+
+$req_subscribe = "SELECT COUNT(*) FROM users WHERE pseudo='".$pseudo."'";
 
 $sql = "INSERT INTO users (id_user, pseudo, email, firstname, name, note, password)
 VALUES (NULL, '".$pseudo."','".$email."','".$firstname."', '".$name."', '2.5', '".$password."')";
