61540eac1d618ed3d9964b74c7b98787a4968b98
root <guillaume.cyril.zisa@gmail.com>
2018-06-24 02:04:11 +0200

    More style <3

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<script src="view/bootstrap1.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"></script>
+    <script src="view/bootstrap2.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"></script>
+    <script src="view/bootstrap3.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" ></script>
+    <link rel="stylesheet" href="view/bootstrap.css" >
 		<link rel='stylesheet' type='text/css' href='view/style.css'>
-		<title>TerraBay </title>
+		<title>TerraBay</title>
 	</head>
 	<body>
 		<?php if (isset($_GET["subscribe"]) && $_GET["subscribe"] == "confirmed"){ echo "<script>alert('Your account has been created !')</script>";} ?>
 		<center><h1> Welcome on TerraBay</h1></center>
-		<form action="controller/index_controller.php" method="POST">
-			<button name="go" value="login">Login</button>
-		</form>
-		<form action="controller/index_controller.php" method="POST">
-			<button name="go" value="subscribe">Subscribe</button>
-		</form>
+		<br><br>
+		<div id="box">
+			<br>
+			<h3>Please login or register to access the services.</h3>
+			<form action="controller/index_controller.php" method="POST">
+				<button class="button button1" name="go" value="login">Login</button>
+			</form>
+			<form action="controller/index_controller.php" method="POST">
+				<button class="button button2" name="go" value="subscribe">Subscribe</button>
+			</form>
+			<br><br>
+		</div>
 		<?php
-			if ( isset($_GET["user"])){
-				if ( $_GET["user"] === "exist"){
-					echo "<h6> Your pseudo already exist ! </h6>";
-				}
-			}
+			if ( isset($_GET["user"])){if ( $_GET["user"] === "exist"){echo "<h6> Your pseudo already exist ! </h6>";}}
 		?>
 	</body>
 </html>
--- view/login.php
+++ view/login.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel='stylesheet' type='text/css' href='view/style.css'>
+		<link rel='stylesheet' type='text/css' href='../view/style.css'>
 		<title>TerraBay </title>
 	</head>
 	<body>
+
     <form action="../index.php" method="POST">
-      <button>Back</button>
+      <button class="button button2" >Back</button>
     </form>
-    <h2>Login Field</h2>
-    <form action="login.php" method="POST">
-      <label for="pseudo">Username : </label><input type="text" size="30px"name="pseudo" value=""><br>
-      <label for="password">Password : </label><input type="password" size="30px"name="password" value=""><br>
+
+		<center><h2>Login Field</h2></center>
+		<div id="box1">
+			<br><br>
+		<form action="login.php" method="POST">
+      <label for="pseudo">Username : </label><input type="text" size="20px"name="pseudo" value=""><br>
+      <label for="password">Password : </label><input type="password" size="20px"name="password" value=""><br>
       <?php if (isset ($_GET["error"])){echo "Your username or password is wrong .<br>";}else{echo"<br>";} ?>
-      <button>Submit</button>
+      <button class="button button1">Submit</button>
     </form>
+		<br><br>
+	</div>
   </body>
 </html>
