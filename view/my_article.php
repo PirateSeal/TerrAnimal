7043e4ef149d95da163884cad37111a998275417
root <guillaume.cyril.zisa@gmail.com>
2018-06-24 12:49:37 +0200

    add_article

--- view/my_article.php
+++ view/my_article.php
@@ -2,21 +2,25 @@
 	echo "<center><h2>Welcome ".$_SESSION["pseudo"]."</h2></center>";
 ?>
 
-<div id="box2">
+<div id="box3">
 
 <?php
 	if (isset($data)) {
-		echo "<br>";
+		$x=0;
 		for ($i=0; $i <count($data) ; $i++) {
-			echo"<br><table border = 1px><tr><td>image</td></tr>
-			<tr><td>".$data[$i]['name']."</td></tr>
-			<tr><td>Price :".$data[$i]['unit_price']." $</td></tr>
-			<tr><td><form action='../controller/modify_article.php?id=".$data[$i]['id_article']."' method='POST'>
-			<button> Modify </button></form></td></tr>
-			<tr><td><form action='../controller/delete_article.php?id=".$data[$i]['id_article']."' method='POST'>
-			<button> Delete </button></form></td></tr></table><br>";
+			echo "<center><div class='col-sm-3' style='background-color:white; opacity:0.7; color:red; border:solid black 2px ;overflow: hidden;max-width: 336px; '><br>
+			<img class='float-left mr-5' src = '".$data[$i]['photo_path']."' width='25%'>
+			<br>".$data[$i]['name']."<br>".$data[$i]['unit_price']." $<br>
+			<form action='../controller/modify_article.php?id=".$data[$i]['id_article']."' method='POST'>
+			<input type='submit' class='btn btn-dark' style='background-color:dark;'  value='Modify'></form>
+			<form action='../controller/delete_article.php?id=".$data[$i]['id_article']."' method='POST'>
+			<input type='submit' class='btn btn-danger' style='background-color:dark;' value='Delete'></form><br>
+			</div></center>";
+			$x++;
+			if ( $x === 3 ){
+				echo "<br>";
+			}
 		}
-		echo "<br>";
 	} else {
 		echo "<br>You don't had any article !<br>Click <a href='../controller/add_article_display.php'>here</a> to add one .<br><br>";
 	}
