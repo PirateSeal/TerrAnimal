425813a455456ce0c798b43c85ec52776521d177
root <guillaume.cyril.zisa@gmail.com>
2018-06-24 20:39:37 +0200

    Add_Money

---
+++ controller/controller_monney.php
@@ -0,0 +1,21 @@
+<?php
+ require_once("../controller/loged_or_not.php");
+ require_once("../controller/db_connexion.php");
+ include("../view/header.php");
+echo "<center><h2>Adding credits</h2></center>";
+ if (isset($_GET["add"])){
+  $add = $_GET["add"]+$_SESSION["monney"];
+  $req_add_monney = "UPDATE users SET balance='".$add."' WHERE id_user='".$_SESSION['ID']."'";
+  $req_monney = $db_connexion->prepare($req_add_monney);
+  $req_monney->execute();
+  $_SESSION["monney"] = $add;
+  header("refresh:5 ; url=../controller/controller_monney.php");
+  echo "<div id='box'><br>You account will be accredited to ".$_GET['add']." in 5 seconds.<br><br></div>";
+ }else {
+  echo "<div id='box'><br>
+        <form action='../controller/controller_monney.php' method='GET'>
+        <input type='text' name='add' value=''>
+        <button class='button1'>Add money</button>
+        <br><br></div>";
+ }
+?>
