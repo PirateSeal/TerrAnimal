69a531929a8c0bc5d1e9458d718ff67f9c5baa05
SibreuR <erwann1998@gmail.com>
2018-06-28 23:19:54 +0200

    repair_note

--- controller/user_account.php
+++ controller/user_account.php
@@ -9,36 +9,24 @@
 			require_once("../model/user_account.php");
 			require_once("../view/user_account.php");
 		} else {
-			echo "1";
 			if (isset($_GET['id_transa'])) {
-				echo "oui";
 				$transa = $_GET['id_transa'];
 			} elseif (isset($_POST['id_transa'])) {
-				echo "non";
-				$transa = $_GET['id_transa'];
+				$transa = $_POST['id_transa'];
 			}
 			require_once("../model/user_account_data.php");
 			$iduser = $seller;
 
-			if (isset($_POST['vote']) && $_POST['vote'] == "oui") {
-			echo "2";
-			$transa = $_POST['id_transa'];
-			$vote = $_POST['note'];
-			require_once("../model/vote.php");
+			if (isset($_GET['vote']) && $_GET['vote'] == "oui") {
+				$transa = $_POST['id_transa'];
+				$vote = $_POST['note'];
+				require_once("../model/vote.php");
+				require_once("../model/user_account.php");
+				require_once("../view/user_account.php");
 			} else {
-			require_once("../model/user_account.php");
-			require_once("../view/user_account.php");
+				require_once("../model/user_account.php");
+				require_once("../view/user_account.php");
 			}
 		}
-
-/*		if (isset($_POST['vote']) && $_POST['vote'] == "oui") {
-			echo "2";
-			$transa = $_GET['id_transa'];
-			$vote = $_POST['note'];
-			require_once("../model/vote.php");
-		} else {
-		require_once("../model/user_account.php");
-		require_once("../view/user_account.php");
-		}*/
 	}
 ?>
