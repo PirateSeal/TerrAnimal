bbc435b73505546035edb7bb82cc5992bb905106
root <guillaume.cyril.zisa@gmail.com>
2018-07-01 17:53:31 +0200

    update bids

--- controller/controller_bids.php
+++ controller/controller_bids.php
@@ -3,6 +3,7 @@ require("../controller/loged_or_not.php");
 require("../controller/db_connexion.php");
 require("../view/header.php");
 
+if (!isset($_GET["id_bid"]) && !isset($_GET["bid"])){
 $year = date("Y");
 $month = date("m");
 $day = date("d");
@@ -10,6 +11,8 @@ $hour = date("H");
 $minute = date("i");
 $second = date("s");
 echo $year."-".$month."-".$day." ".$hour.":".$minute.":".$second;
+
+
 $req_user =" SELECT * FROM articles WHERE status='bid'";
 $data = $db_connexion->query($req_user)->fetchAll();
 
@@ -21,6 +24,13 @@ for ($i=0; $i < count($data) ; $i++) {
 
     $req_specie = "select name from species where id_specie='".$data[$i][1]."'";
     $specie = $db_connexion->query($req_specie)->fetch();
+
+    $full_date_end = explode(" ", $bid[4]);
+    $date_end = explode("-",$full_date_end[0]);
+    $hour_end = explode(":",$full_date_end[1]);
+
+    echo $date_end[2];
+    if ( $date_end[0] >= $year || $date_end[0] >= $year && $date_end[1] >= $month || $date_end[0] >= $year && $date_end[1] >= $month && $date_end[2] >= $day ) {
 
     echo "Description : ".$data[$i][3]."<br>";
     echo "<img src=".$data[$i][13] ."><br>";
@@ -37,7 +47,36 @@ for ($i=0; $i < count($data) ; $i++) {
     echo "End price :".$bid[6]."<br>";
     echo "Date start :".$bid[3]."<br>";
     echo "Date end :".$bid[4]."<br><br>";
-
+    echo "<form action='../controller/controller_bids.php' method='GET'><input type='hidden' name='id_bid' value='".$bid[0]."'><button class='button2'>Add an offer to the bid</button></form>";
+  } else {
+    echo "Désactivation de l'enchère et debitation du compte acheteur";
+  }
 }
 echo "<br><br><br></div>";
+}else if ( isset($_GET['bid']) && isset($_GET['id_bid'])){
+  $req_bid =" SELECT * FROM bids WHERE id_bi='".$_GET['id_bid']."'";
+  $bid = $db_connexion->query($req_bid)->fetch();
+  $price = $bid[6]+1;
+
+  if( $_GET['bid'] < $price ){
+    header("location:../controller/controller_bids.php?error=price&id_bid=$_GET[id_bid]");
+  } else {
+    echo "ALGO AJOUT DU NOOUVEAU PRIX ET CLIENT";
+  }
+}else {
+    $req_bid =" SELECT * FROM bids WHERE id_bi='".$_GET['id_bid']."'";
+    $bid = $db_connexion->query($req_bid)->fetch();
+    $price = $bid[6]+1;
+    echo "<h2><center>Enter your offer</center></h2><div id='box2'>The minimum price is ".$price."
+    <form action='controller_bids.php' method='GET'>
+    <input type='number' name='bid' value=''>
+    <input type='hidden' name='id_bid' value='".$_GET['id_bid']."'>";
+
+    if ( isset($_GET["error"])){
+      echo "<br>Please enter a price more expensive than the actual offer .<br>";
+    }
+
+    echo"<button class='button1'>Add this offer</button>
+    <form>";
+  }
 ?>
