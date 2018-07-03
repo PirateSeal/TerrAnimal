0cdefcafaf181effe7e9c6df8dc9a858f6701cf6
root <guillaume.cyril.zisa@gmail.com>
2018-07-03 16:50:58 +0200

    update_monney_some_inserts_display_home

--- controller/controller_monney.php
+++ controller/controller_monney.php
@@ -4,17 +4,24 @@
  include("../view/header.php");
 echo "<center><h2>Adding credits</h2></center>";
  if (isset($_GET["add"])){
-  $add = $_GET["add"]+$_SESSION["monney"];
-  $req_add_monney = "UPDATE users SET balance='".$add."' WHERE id_user='".$_SESSION['ID']."'";
-  $req_monney = $db_connexion->prepare($req_add_monney);
-  $req_monney->execute();
-  $_SESSION["monney"] = $add;
-  header("refresh:5 ; url=../controller/controller_monney.php");
-  echo "<div id='box'><br>You account will be accredited to ".$_GET['add']." in 5 seconds.<br><br></div>";
- }else {
+   if (is_numeric($_GET["add"])) {
+    $add = $_GET["add"]+$_SESSION["monney"];
+    $req_add_monney = "UPDATE users SET balance='".$add."' WHERE id_user='".$_SESSION['ID']."'";
+    $req_monney = $db_connexion->prepare($req_add_monney);
+    $req_monney->execute();
+    $_SESSION["monney"] = $add;
+    header("refresh:5 ; url=../controller/controller_monney.php");
+    echo "<div id='box'><br>You account will be accredited to ".$_GET['add']." in 5 seconds.<br><br></div>";
+  }else {
+    header("location:../controller/controller_monney.php?error=number");
+  }
+}else {
+  if (isset($_GET["error"])){
+    echo "Please enter a number";
+  }
   echo "<div id='box'><br>
         <form action='../controller/controller_monney.php' method='GET'>
-        <input type='text' name='add' value=''>
+        <input type='number' name='add' value=''>
         <button class='button1'>Add money</button>
         <br><br></div>";
  }
--- view/home_view.php
+++ view/home_view.php
@@ -6,16 +6,16 @@
 	$x=0;
 
 		if (isset($data)) {
-			echo "<div class='row ml-5'>";
+			echo "<div class='row ml-4 center'>";
 			for ($i=0; $i <count($data) ; $i++) {
 				echo "<div class='col-sm-3' style='background-color:white; opacity:0.7; color:red; border:solid black 2px ;overflow: hidden;max-width: 336px; '><br>
-				<img class='float-left mr-5' src = '".$data[$i]['photo_path']."' width='25%'>
-				<br>".$data[$i]['name']."<br>".$data[$i]['unit_price']." $<br>
+				<img class='float-left mr-5 mt-2' src = '".$data[$i]['photo_path']."' height='120px' width='120px'>
+				<br>".$data[$i]['description']."<br>".$data[$i]['name']."<br>".$data[$i]['unit_price']." $<br>
 				<form action='../controller/product_page_controller.php?id=".$data[$i]['id_article']."' method='POST'>
 				<input type='hidden' name='display' value='article'>
 				<input type='submit' class='btn btn-dark ' style='background-color:dark;' name='Details' value='Details'></form><br><br></div>";
 				$x++;
-				if ($x%3==0){
+				if ($x%2==0){
 					echo "<br>";
 				}
 			}
