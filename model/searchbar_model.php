7addd7d3f11a5de36a71431e9eb1c038f7cde37d
root <guillaume.cyril.zisa@gmail.com>
2018-07-04 14:44:51 +0200

    Correction

--- model/searchbar_model.php
+++ model/searchbar_model.php
@@ -4,7 +4,7 @@
 	$sql ='select id_user,balance from users where pseudo = "'.$_SESSION['pseudo'].'"';
 	$req = $db_connexion->query($sql);
 	$user_data = $req->fetch();
-	$sql = 'select id_article, unit_price, species.name, articles.photo_path from users inner join articles on users.id_user = articles.id_user inner join species on articles.id_specie = species.id_specie where users.id_user != "'.$user_data['id_user'].'"and articles.status="available" and species.name like "%'.$_POST['search'].'%"';
+	$sql = 'select id_article, description,unit_price, species.name, articles.photo_path from users inner join articles on users.id_user = articles.id_user inner join species on articles.id_specie = species.id_specie where users.id_user != "'.$user_data['id_user'].'"and articles.status="available" and species.name like "%'.$_POST['search'].'%"';
 	$req = $db_connexion->query($sql);
 
 	$i=0;
