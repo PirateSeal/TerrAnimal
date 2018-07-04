164cbcdbeb019a5b60512101fc87834358e53357
root <guillaume.cyril.zisa@gmail.com>
2018-07-04 16:19:13 +0200

    update...

--- controller/user_account.php
+++ controller/user_account.php
@@ -1,32 +1,31 @@
 <?php
-	SESSION_start();
-	if (empty($_SESSION["pseudo"])){
-		header("location:../index.php");
-	}else{
-		include("../view/header.php");
-		if (isset($_GET['id_user'])) {
-			$iduser = $_GET['id_user'];
+	require("../controller/loged_or_not.php");
+	include("../view/header.php");
+	if (isset($_GET['id_user'])) {
+		$iduser = $_GET['id_user'];
+		require_once("../model/user_account.php");
+		require_once("../view/user_account.php");
+	} else {
+		if (isset($_GET['id_transa'])) {
+			$transa = $_GET['id_transa'];
+		} elseif (isset($_POST['id_transa'])) {
+			$transa = $_POST['id_transa'];
+		}
+		require_once("../model/user_account_data.php");
+		$iduser = $seller;
+		if (isset($_GET['vote']) && $_GET['vote'] == "oui") {
+			$transa = $_POST['id_transa'];
+			$vote = $_POST['note'];
+			require_once("../model/vote.php");
 			require_once("../model/user_account.php");
 			require_once("../view/user_account.php");
 		} else {
-			if (isset($_GET['id_transa'])) {
-				$transa = $_GET['id_transa'];
-			} elseif (isset($_POST['id_transa'])) {
-				$transa = $_POST['id_transa'];
-			}
-			require_once("../model/user_account_data.php");
-			$iduser = $seller;
 
-			if (isset($_GET['vote']) && $_GET['vote'] == "oui") {
-				$transa = $_POST['id_transa'];
-				$vote = $_POST['note'];
-				require_once("../model/vote.php");
-				require_once("../model/user_account.php");
-				require_once("../view/user_account.php");
-			} else {
-				require_once("../model/user_account.php");
-				require_once("../view/user_account.php");
-			}
+			require_once("../model/user_account.php");
+			echo "<center><h2>Ranking you seller .</h2></center>";
+			echo "<div id='box2'><br><center>";
+			require_once("../view/user_account.php");
+			echo "</div></center><br><br>";
 		}
 	}
 ?>
--- view/user_account.php
+++ view/user_account.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>User Page</title>
-</head>
-<body>
 	<?php
-		echo "<table>
-		<tr><td>Vendor Image</td><td></td></tr>
-		<tr><td>Pseudo : ".$data['pseudo']."</td> <td>Name : ".$data['firstname']." ".$data['name']."</td></tr>
-		<tr><td>Mail : ".$data['email']."</td> <td></td></tr>
-		<tr><td>Note : ".$data['note']."/5</td><td>";
+		echo "<div id='box3'><table>
+		Seller pseudo : ".$data['pseudo']."<br>
+		Firstname : ".$data['firstname']."<br>
+		Name :".$data['name']."<br>
+		Mail : ".$data['email']."
+		Note : ".$data['note']."/5";
 		if (isset($_GET['id_transa'])) {
-		 	echo "<form action='../controller/user_account.php?vote=oui' method='POST'>
+		 	echo "<div id='box3'><form action='../controller/user_account.php?vote=oui' method='POST'>
 		 	<input id='note' type='number' name='note' step='0.5' min='0' max='5' value=''>
 		 	<input id='id_transa' name='id_transa' type='hidden' value='".$_GET['id_transa']."'>
-		 	<button>Vote</button></form>";
+		 	<button>Vote</button></form></div>";
 		 }
 		 echo "</td></tr>
 		</table>";
